86b121931864e8adf9bc5c397eca54a72e300dad
Glaucio Fonseca <glauciofonseca26@gmail.com>
2015-02-26 00:16:55 -0300

    Create dinamicas.py

---
+++ dinamicas.py
@@ -0,0 +1,25 @@
+a = 1
+b = 2
+c = 3
+d = 4
+print("O valor de A é: %d, python e show" %a)
+print("O valor de B é: %d, python e show" %b)
+print("O valor de C é: %d, python e show" %c)
+print("O valor de D é: %d, python e show" %d)
+
+#trocando valores
+a,b = b,a = c,d
+print("VIVA O PYTHON HAHAHA")
+#imprimindo valores trocados
+print("O valor de A é: %d" %a)
+print("O valor de B é: %d" %b)
+print("O valor de C é: %d" %c)
+print("O valor de D é: %d" %d)
+
+#imprimindo uma palavra com uma letra em cada variável
+a,b,c,d,e = "Zirou"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
