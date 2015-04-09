345a68bdeab84f2da9011beeb0668f6e8564d342
Zirou <z3rou@hotmail.com>
2015-04-09 19:29:09 -0300

    Create shellencode.py

---
+++ shellencode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import binascii, sys, time
+
+def main():
+    print "Shell Encode"
+    inputtype = raw_input("Por favor insera os dados: ")
+    print "shellcode => ",
+    for encoded in inputtype:
+        print "\b\\x"+encoded.encode("hex"),
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    main()
